8b45925f0cca6df2b603bce08ec19fdc94837700
Minh Vương <hardingadonis@gmail.com>
2024-09-26 20:41:06 +0700

    fix: fix swagger payload

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,68 +1,77 @@
 import { Injectable } from '@nestjs/common';
+import { ApiProperty } from '@nestjs/swagger';
 
-export interface Book {
-    id: string;
-    title: string;
-    description: string;
-    author: string;
-    publishedYear: number;
+export class Book {
+	id: string;
+
+	@ApiProperty()
+	title: string;
+
+	@ApiProperty()
+	description: string;
+
+	@ApiProperty()
+	author: string;
+
+	@ApiProperty()
+	publishedYear: number;
 }
 
 @Injectable()
 export class BookService {
-    private books = [
-        {
-            id: "book0001",
-            title: 'To Kill a Mockingbird',
-            description:
-                'A novel set in the Depression-era South, focusing on issues of race and injustice as seen through the eyes of a young girl named Scout Finch.',
-            author: 'Harper Lee',
-            publishedYear: 1960,
-        },
-        {
-            id: "book0002",
-            title: '1984',
-            description:
-                'A dystopian novel that explores the dangers of totalitarianism, surveillance, and censorship, set in a society ruled by the Party and its leader, Big Brother.',
-            author: 'George Orwell',
-            publishedYear: 1949,
-        },
-        {
-            id: "book0003",
-            title: 'Pride and Prejudice',
-            description:
-                'A classic romantic novel that follows Elizabeth Bennet as she navigates issues of manners, upbringing, morality, and marriage in the society of early 19th-century England.',
-            author: 'Jane Austen',
-            publishedYear: 1813,
-        },
-    ];
+	private books = [
+		{
+			id: 'book0001',
+			title: 'To Kill a Mockingbird',
+			description:
+				'A novel set in the Depression-era South, focusing on issues of race and injustice as seen through the eyes of a young girl named Scout Finch.',
+			author: 'Harper Lee',
+			publishedYear: 1960,
+		},
+		{
+			id: 'book0002',
+			title: '1984',
+			description:
+				'A dystopian novel that explores the dangers of totalitarianism, surveillance, and censorship, set in a society ruled by the Party and its leader, Big Brother.',
+			author: 'George Orwell',
+			publishedYear: 1949,
+		},
+		{
+			id: 'book0003',
+			title: 'Pride and Prejudice',
+			description:
+				'A classic romantic novel that follows Elizabeth Bennet as she navigates issues of manners, upbringing, morality, and marriage in the society of early 19th-century England.',
+			author: 'Jane Austen',
+			publishedYear: 1813,
+		},
+	];
 
-    async getAllBooks(): Promise<Book[]> {
-        return this.books;
-    }
+	async getAllBooks(): Promise<Book[]> {
+		return this.books;
+	}
 
-    async getBookById(id: string): Promise<Book> {
-        console.log(`getBookById called with id: ${id}`);
-        const book = this.books.find((book) => book.id === id);
-        if (!book) {
-            console.log(`Book with id ${id} not found`);
-        }
-        return book;
-    }
+	async getBookById(id: string): Promise<Book> {
+		console.log(`getBookById called with id: ${id}`);
+		const book = this.books.find((book) => book.id === id);
+		if (!book) {
+			console.log(`Book with id ${id} not found`);
+		}
+		return book;
+	}
 
-    async createBook(book: Book): Promise<Book> {
-        this.books.push(book);
+	async createBook(book: Book): Promise<Book> {
+		this.books.push(book);
 
-        return book;
-    }
+		return book;
+	}
 
-    async updateBook(id: string, book: Book): Promise<Book> {
-        const index = this.books.findIndex((book) => book.id === id);
-        this.books[index] = book;
-        return book;
-    }
+	async updateBook(id: string, book: Book): Promise<Book> {
+		const index = this.books.findIndex((book) => book.id === id);
+		this.books[index] = { ...this.books[index], ...book };
+		return book;
+	}
 
-    async deleteBook(id: string): Promise<void> {
-        this.books = this.books.filter((book) => book.id !== id);
-    }
+	async deleteBook(id: string): Promise<void> {
+		this.books = this.books.filter((book) => book.id !== id);
+	}
 }
