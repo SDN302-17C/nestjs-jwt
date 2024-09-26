296af8d8700da6e54f666c1468074ef4934f53b5
Đinh Quốc Chương <quocchuong2609@gmail.com>
2024-09-26 20:23:52 +0700

    add: crud api for book

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Book, BookService } from './book.service';
 
-@Controller('book')
-export class BookController {}
+@Controller('books')
+export class BookController {
+    constructor(private readonly bookService: BookService) {}
+
+    @Get()
+    getAllBooks(): Promise<Book[]> {
+        return this.bookService.getAllBooks();
+    }
+
+    @Get(':id')
+    getBookById(@Param('id') id: string): Promise<Book> {
+        return this.bookService.getBookById(id);
+    }
+
+    @Post()
+    createBook(@Body() book: Book): Promise<Book> {
+        return this.bookService.createBook(book);
+    }
+
+    @Put(':id')
+    updateBook(@Param('id') id: string, @Body() book: Book): Promise<Book> {
+        return this.bookService.updateBook(id, book);
+    }
+
+    @Delete(':id')
+    deleteBook(@Param('id') id: string): Promise<void> {
+        return this.bookService.deleteBook(id);
+    }
+
+}
--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,4 +1,68 @@
 import { Injectable } from '@nestjs/common';
 
+export interface Book {
+    id: string;
+    title: string;
+    description: string;
+    author: string;
+    publishedYear: number;
+}
+
 @Injectable()
-export class BookService {}
+export class BookService {
+    private books = [
+        {
+            id: "book0001",
+            title: 'To Kill a Mockingbird',
+            description:
+                'A novel set in the Depression-era South, focusing on issues of race and injustice as seen through the eyes of a young girl named Scout Finch.',
+            author: 'Harper Lee',
+            publishedYear: 1960,
+        },
+        {
+            id: "book0002",
+            title: '1984',
+            description:
+                'A dystopian novel that explores the dangers of totalitarianism, surveillance, and censorship, set in a society ruled by the Party and its leader, Big Brother.',
+            author: 'George Orwell',
+            publishedYear: 1949,
+        },
+        {
+            id: "book0003",
+            title: 'Pride and Prejudice',
+            description:
+                'A classic romantic novel that follows Elizabeth Bennet as she navigates issues of manners, upbringing, morality, and marriage in the society of early 19th-century England.',
+            author: 'Jane Austen',
+            publishedYear: 1813,
+        },
+    ];
+
+    async getAllBooks(): Promise<Book[]> {
+        return this.books;
+    }
+
+    async getBookById(id: string): Promise<Book> {
+        console.log(`getBookById called with id: ${id}`);
+        const book = this.books.find((book) => book.id === id);
+        if (!book) {
+            console.log(`Book with id ${id} not found`);
+        }
+        return book;
+    }
+
+    async createBook(book: Book): Promise<Book> {
+        this.books.push(book);
+
+        return book;
+    }
+
+    async updateBook(id: string, book: Book): Promise<Book> {
+        const index = this.books.findIndex((book) => book.id === id);
+        this.books[index] = book;
+        return book;
+    }
+
+    async deleteBook(id: string): Promise<void> {
+        this.books = this.books.filter((book) => book.id !== id);
+    }
+}
