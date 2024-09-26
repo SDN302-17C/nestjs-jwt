4416546783094ebac3c6783380298603cb88784c
Đinh Quốc Chương <quocchuong2609@gmail.com>
2024-09-26 19:44:37 +0700

    add: base nestjs

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { BookModule } from './book/book.module';
+
+@Module({
+	imports: [AuthModule, UsersModule, BookModule],
+	controllers: [AppController],
+	providers: [AppService],
+})
+export class AppModule {}
