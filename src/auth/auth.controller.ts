ca54e2e4f385f1edd4381724750d2fec25b50ac5
Minh Vương <hardingadonis@gmail.com>
2024-09-26 20:36:37 +0700

    fix: fix authen all api

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,12 +2,14 @@ import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { ApiTags } from '@nestjs/swagger';
 import { LoginDto } from './dto/login.dto';
+import { Public } from './auth.guard';
 
 @ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
 	constructor(private authService: AuthService) {}
 
+	@Public()
 	@HttpCode(HttpStatus.OK)
 	@Post('login')
 	signIn(@Body() signInDto: LoginDto) {
